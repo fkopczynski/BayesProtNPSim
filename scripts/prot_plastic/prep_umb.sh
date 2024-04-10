0013559d28273576d4c88201e4515f0fa23130d8
fkopczynski <fjkopczynski@gmail.com>
2024-04-10 16:02:10 +0200

    Increased conciseness of the script

--- scripts/prot_plastic/prep_umb.sh
+++ scripts/prot_plastic/prep_umb.sh
@@ -7,18 +7,31 @@ cp ../md/md.cpt .
 # prepare the tpr file
 gmx convert-tpr -s md.tpr -extend 75000 -o 75ns.tpr
 
+# loop over umbrellas and prepare each system
 for i in $(seq 1.6 0.5 6.6); do
 mkdir $i
 cd $i
 cp ../75ns.tpr .
-cp ../md.cpt .
-cp ~/project/plumed_files/$i/plumed.dat . 
-cp ~/project/plumed_files/*gpu_umbrella .
-cp ~/project/plumed_files/simulate_plumed.sh .
-sed -i "2s/.*/#SBATCH --job-name=u${i}/" 16gpu_umbrella
-sed -i "2s/.*/#SBATCH --job-name=u${i}/" 20gpu_umbrella
-sed -i "2s/.*/#SBATCH --job-name=u${i}/" 24gpu_umbrella
-sed -i "2s/.*/#SBATCH --job-name=u${i}/" 32gpu_umbrella
+cp ../md.cpt . 
+cp ~/project/plumed_files/* .
+
+# set plumed.dat, velocity of pulling set to 0.35 nm/ns
+# save calculated params for debugging
+vel=0.35
+timestep_ns=0.000002
+restart_step=50000000
+dist=$(echo "${i} - 1.6" | bc)
+echo "distance to pull: ${dist}" > params.log
+time_ns=$(echo "scale=3; ${dist}/${vel}" | bc)
+echo "time in ns: ${time_ns}" >> params.log
+step=$(echo "${restart_step} + ${time_ns}/${timestep_ns}" | bc)
+echo "step: ${step}" >> params.log
+sed -i "15s/.*/   STEP1=${step}  AT1=${i}/" plumed.dat
+
+# set job names
+sed -i "2s/.*/#SBATCH --job-name=u${i}/" *gpu_umbrella
 cd ../
 done
-rm 75ns.tpr md.cpt
+
+# clean up
+rm 75ns.tpr md.cpt md.tpr
