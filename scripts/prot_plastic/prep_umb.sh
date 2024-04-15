e1c4e340733fd252e39ac226009f5216175398a0
fkopczynski <fjkopczynski@gmail.com>
2024-04-15 17:03:26 +0200

    Preparation script for umbrella sampling

--- scripts/prot_plastic/prep_umb.sh
+++ scripts/prot_plastic/prep_umb.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# script preparing the system for umbrella sampling
+# assumes that umbrella sampling is indended to last 75 ns
+# requires one command line parameter which defines the value of COM separation to sample
+
 # copy the tpr file
 cp ../md/md.tpr .
 cp ../md/md.cpt .
@@ -8,9 +12,8 @@ cp ../md/md.cpt .
 gmx convert-tpr -s md.tpr -extend 75000 -o 75ns.tpr
 
 # loop over umbrellas and prepare each system
-for i in $(seq 1.6 0.5 6.6); do
-mkdir $i
-cd $i
+mkdir $1
+cd $1
 cp ../75ns.tpr .
 cp ../md.cpt . 
 cp ~/project/plumed_files/* .
@@ -20,16 +23,17 @@ cp ~/project/plumed_files/* .
 vel=0.35
 timestep_ns=0.000002
 restart_step=50000000
-dist=$(echo "${i} - 1.6" | bc)
+dist=$(echo "$1 - 1.6" | bc)
 echo "distance to pull: ${dist}" > params.log
 time_ns=$(echo "scale=3; ${dist}/${vel}" | bc)
 echo "time in ns: ${time_ns}" >> params.log
 step=$(echo "${restart_step} + ${time_ns}/${timestep_ns}" | bc)
 echo "step: ${step}" >> params.log
-sed -i "15s/.*/   STEP1=${step}  AT1=${i}/" plumed.dat
+sed -i "15s/.*/   STEP1=${step}  AT1=$1/" plumed.dat
 
 # set job names
-sed -i "2s/.*/#SBATCH --job-name=u${i}/" *gpu_umbrella
+sed -i "2s/.*/#SBATCH --job-name=u$1/" *gpu_umbrella
+sbatch 16gpu_umbrella
 cd ../
 done
 
