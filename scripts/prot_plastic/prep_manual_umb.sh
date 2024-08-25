80f7ded7d8311eb5b9aa8cdc3038b95a6cb3d2d6
Filip Kopczyński <fjkopczynski@gmail.com>
2024-08-25 17:31:21 +0200

    Script to prepare for manual full-profile umbrella sampling

--- scripts/prot_plastic/prep_manual_umb.sh
+++ scripts/prot_plastic/prep_manual_umb.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# This script prepares the equilibrated protein+plastic system for a series of 12x 75 ns umbrella sampling runs to generate a full free-energy profile for a given system
+# It is not included in the optimiser and should be used manually.
+# Important! Remember to change the project path, otherwise required wiles will not be copied
+# Time-related parameters will be saved in params.log file
+
+# path to cloned repo
+proj_path="$HOME/project"
+
+# pulling velocity
+vel=0.35
+
+# timestep in ns
+timestep_ns=0.000002
+
+# step at which equilibration is completed and restarted for umbrella sampling
+restart_step=50000000
+
 # copy the tpr file
 cp ../md/md.tpr .
 cp ../md/md.cpt .
@@ -13,13 +30,10 @@ mkdir $i
 cd $i
 cp ../75ns.tpr .
 cp ../md.cpt . 
-cp ~/project/plumed_files/* .
+cp $proj_path/plumed_files/* .
 
-# set plumed.dat, velocity of pulling set to 0.35 nm/ns
+# set plumed.dat
 # save calculated params for debugging
-vel=0.35
-timestep_ns=0.000002
-restart_step=50000000
 dist=$(echo "${i} - 1.6" | bc)
 echo "distance to pull: ${dist}" > params.log
 time_ns=$(echo "scale=3; ${dist}/${vel}" | bc)
