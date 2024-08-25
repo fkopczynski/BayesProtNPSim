755a21ec94cf117dce6476a9be15c91ede24447d
Filip Kopczyński <fjkopczynski@gmail.com>
2024-08-25 15:56:57 +0200

    Script for generating and equilibrating the plastic

--- scripts/plastic/gen.sh
+++ scripts/plastic/gen.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
 
-# some defaults
+# This script generates a plastic molecule of a given length, prepares it for simulation in GROMACS and runs an equilibration. It also sets up the required results folders and copies required files for the next steps. Make sure that you have the project repo cloned and change your local path to it below so that the next steps are set up correctly..
+# Needed files (must be placed in the same directory as this script):
+# - .mol2 files in which the monomers are defined
+# - mainchain files in which the head, middle and tail residues are defined (3 files per monomer, examples included)
+# Parameters:
+# -p: plastic name, e.g. ps [REQUIRED]
+# -l: plastic length [REQUIRED]
+# -d: distance between the molecule and the edge of the box, 1.5 nm by default
+# -c: concentration of NaCl, by default 150 mM
+# -r: molecules which are replaced with ions during GROMACS pre-processing, by default SOL
+# the output of GROMACS operations is a prep.log file which contains the outputs and potential error messages of GROMACS commands
+
+# path to the cloned repo
+# change according to your placement of the folder!
+proj_path="$HOME/project/"
+
+# default names of the parameters
 dist=1.5
 ion_conc=0.15
 ion_repl='SOL'
 plast=""
 len=0
+
+# path to the GROMACS executable
+gmx_mpi="/home/spack-user/spack/opt/spack/linux-centos7-zen3/aocc-3.1.0/gromacs-2020.4-z7lmmyeup2uhxfy2mr3bwi2dt6k4grzy/bin/gmx_mpi"
 
 # get script parameters
 while getopts "p:l:d:c:r:" opt; do
@@ -20,14 +39,13 @@ case $opt in
 esac
 done
 
-echo ${plast}
-
 # check if plastic and its length are given because they are essential, otherwise exit
 if [ -z $plast ] || [ $len -eq 0 ]; then
 echo "Arguments -p and -l must be given"
 exit 1
-elif [ $plast != "ps" && $plast != "idk" && $plast != "pm"]; then 
-echo "Please choose -p between ps, ?, pm"
+# here more rules can be added if other plastics are investigated
+elif [ $plast != "ps" ]; then 
+echo "Please choose -p between ps, [others?]"
 exit 1
 fi
 
@@ -37,15 +55,11 @@ head1="h${plast}"
 tail1="t${plast}"
 norm1=$plast
 
-# define carboxlated residues
+# define carboxylated residues
 struct2="${plast}_carb"
 head2="h${plast:1:1}c"
 tail2="t${plast:1:1}c"
 norm2="${plast}c"
-
-# activate amber env
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate amber
 
 # prepare standard residues
 antechamber -fi mol2 -fo ac -i ${struct1}.mol2 -o ${struct1}.ac -c bcc -pf y
@@ -62,7 +76,7 @@ prepgen -i ${struct2}.ac -o ${tail2}.prepi -f prepi -m mainchain.${tail2} -rn ${
 rm ${struct2}.ac
 
 # generate the sequence
-# stard with the normal head
+# start with the normal head
 echo "{ ${head1^^}" > poly
 
 # middle residues
@@ -107,39 +121,36 @@ echo "savepdb mol poly.pdb" >> tleap.in
 echo "saveamberparm mol poly.top poly.crd" >> tleap.in
 echo "quit" >> tleap.in
 
-# polymerize anionic
+# polymerize the monomers
 tleap -s -f tleap.in > tleap.out
 
 acpype -p poly.top -x poly.crd -b ${struct1}${len}
 mv ${struct1}${len}.amb2gmx ${struct1}${len}
-
-# deactivate python environment
-conda deactivate
 cd ${struct1}${len}
 
 # GROMACS PREPARATION PART
+
 # create the directories for future computations
 mkdir plastic
 mv * plastic/
 mkdir prot_pl
 cd prot_pl
 mkdir md
-mkdir md/plumed
+mkdir plumed
 
 # put all other required files here
 # set up systems for the next steps
-cp ~/project/protein/* .
-cp ~/project/scripts/prot_pl/put_together.py .
-cp ~/project/scripts/prot_pl/prep_prot_pl.sh .
-cp ~/project/scripts/prot_pl/prep_umb.sh plumed/.
-cp ~/project/scripts/prot_pl/get_force.py plumed/.
+cp $proj_path/protein/* .
+cp $proj_path/scripts/prot_plastic/put_together.py .
+cp $proj_path/scripts/prot_plastic/prep_prot_pl.sh .
+cp $proj_path/scripts/prot_plastic/prep_umb.sh plumed/.
+cp $proj_path/scripts/prot_plastic/get_force.py plumed/.
 
 cd ../plastic/
-cp ~/project/mdps/plastic/* .
-cp ~/project/scripts/plastic/* .
-cp -r ~/project/charmm27.ff .
+cp $proj_path/mdps/plastic/* .
+cp -r $proj_path/charmm27.ff .
 mkdir md
-mv simulate_plastic.sh md.mdp *gpu_submit md/.
+mv simulate_plastic.sh md.mdp md/.
 
 # create the topol.top file
 acp_top=*GMX.top
@@ -159,32 +170,33 @@ awk "NR >= $startline && NR < $stopline" $acp_top > plastic.itp
 
 # put the system in a box
 ac_gro=*.gro
-gmx editconf -f $ac_gro -bt cubic -d $dist -o box.gro &> prep.log
+srun --mpi=pmix $gmx_mpi editconf -f $ac_gro -bt cubic -d $dist -o box.gro &> prep.log
 
 # solvate
-gmx solvate -cs -cp box.gro -p topol.top -o solve.gro &>> prep.log
+srun --mpi=pmix $gmx_mpi solvate -cs -cp box.gro -p topol.top -o solve.gro &>> prep.log
 
 # energy minimisation (emw)
-gmx grompp -f emw.mdp -c solve.gro -p topol.top -o emw.tpr -maxwarn 2 &>> prep.log
-mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm emw -v -ntomp $SLURM_CPUS_PER_TASK >& emw.out
+srun --mpi=pmix $gmx_mpi grompp -f emw.mdp -c solve.gro -p topol.top -o emw.tpr -maxwarn 2 &>> prep.log
+srun --mpi=pmix $gmx_mpi mdrun -deffnm emw -v >& emw.out
 
 #put ion
-gmx grompp -f emw.mdp -c emw.gro -p topol.top -o ion.tpr -maxwarn 1 &>> prep.log
-echo $ion_repl | gmx genion -s ion.tpr -p topol.top -neutral -conc $ion_conc -o ion.gro &>> prep.log
+srun --mpi=pmix $gmx_mpi grompp -f emw.mdp -c emw.gro -p topol.top -o ion.tpr -maxwarn 1 &>> prep.log
+echo $ion_repl | srun --mpi=pmix $gmx_mpi genion -s ion.tpr -p topol.top -neutral -conc $ion_conc -o ion.gro &>> prep.log
 
 # em with ions
-gmx grompp -f em.mdp -c ion.gro -p topol.top -o em.tpr &>> prep.log
-mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm em -v -ntomp $SLURM_CPUS_PER_TASK >& em.out
+srun --mpi=pmix $gmx_mpi grompp -f em.mdp -c ion.gro -p topol.top -o em.tpr &>> prep.log
+srun --mpi=pmix $gmx_mpi mdrun -deffnm em -v >& em.out
 
 # water equilibration (position restraint run)
-gmx grompp -f posre.mdp -c em.gro -r em.gro -p topol.top -o posre.tpr -maxwarn 3 &>> prep.log
-mpirun -np $SLURM_NTASKS gmx_mpi mdrun -deffnm posre -v -ntomp $SLURM_CPUS_PER_TASK >& posre.out
+srun --mpi=pmix $gmx_mpi grompp -f posre.mdp -c em.gro -r em.gro -p topol.top -o posre.tpr -maxwarn 3 &>> prep.log
+srun --mpi=pmix $gmx_mpi mdrun -deffnm posre -v >& posre.out
 
 # production run
 cd md
-gmx grompp -f md.mdp -c ../posre.gro -p ../topol.top -o md.tpr &>> prep.log
+srun --mpi=pmix $gmx_mpi grompp -f md.mdp -c ../posre.gro -p ../topol.top -o md.tpr &>> prep.log
 
-# for generating a consistent job name
-sed -i "2s/.*/#SBATCH --job-name=${norm1}${len}eq/" *gpu_submit
-sbatch 32gpu_submit
+# run the simulation
+srun --mpi=pmix $gmx_mpi mdrun -deffnm md -v >& md.out
+echo 'q' | srun --mpi=pmix $gmx_mpi make_ndx -f md.tpr -o index.ndx
+echo 'Other' | srun --mpi=pmix $gmx_mpi editconf -f md.gro -n index.ndx -o plastic.pdb
 
